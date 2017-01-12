764766ddc137825a66b1ab6e025c94a708cfd6d8
Gerald Holt <dgh21989@hotmail.com>
2017-01-11 19:47:16 -0600

    fix scout movement again and tweak output

--- src/rolesplayer/roles/Scout.java
+++ src/rolesplayer/roles/Scout.java
@@ -54,25 +54,28 @@ public class Scout extends RobotBase {
             if (robotController.canFireSingleShot()) {
                 robotController.fireSingleShot(robotController.getLocation().directionTo(enemyRobots[0].location));
             }
-        } else {
-            if (enemyArchonLoc.x != 0 && enemyArchonLoc.y != 0) {
+        } else if (enemyArchonLoc.x != 0 && enemyArchonLoc.y != 0) {
+            if (rightHanded) {
+                tryMove(robotController, robotController.getLocation().directionTo(enemyArchonLoc).rotateLeftDegrees(90));
+            }
+            if (!robotController.hasMoved()) {
+                tryMove(robotController, robotController.getLocation().directionTo(enemyArchonLoc).rotateRightDegrees(90));
+            }
+            if (!robotController.hasMoved()) {
+                // Move randomly
+                tryMove(robotController, randomDirection());
+            }
+            if (robotController.canFireSingleShot()) {
                 if (rightHanded) {
-                    tryMove(robotController, robotController.getLocation().directionTo(enemyArchonLoc).rotateLeftDegrees(90));
+                    robotController.fireSingleShot(robotController.getLocation().directionTo(enemyArchonLoc).rotateRightDegrees((float) (1 * Math.random())));
+                } else {
+                    robotController.fireSingleShot(robotController.getLocation().directionTo(enemyArchonLoc).rotateLeftDegrees((float) (1 * Math.random())));
                 }
-                if (!robotController.hasMoved()) {
-                    tryMove(robotController, robotController.getLocation().directionTo(enemyArchonLoc).rotateRightDegrees(90));
-                }
-                if (!robotController.hasMoved()) {
-                    // Move randomly
-                    tryMove(robotController, randomDirection());
-                }
-                if (robotController.canFireSingleShot()) {
-                    if (rightHanded) {
-                        robotController.fireSingleShot(robotController.getLocation().directionTo(enemyArchonLoc).rotateRightDegrees((float) (1 * Math.random())));
-                    } else {
-                        robotController.fireSingleShot(robotController.getLocation().directionTo(enemyArchonLoc).rotateLeftDegrees((float) (1 * Math.random())));
-                    }
-                }
+            }
+        } else {
+            if (!robotController.hasMoved()) {
+                // Move randomly
+                tryMove(robotController, randomDirection());
             }
         }
     }

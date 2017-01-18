2534a52f6044312a7720d90b8f0c00eb487b8e9b
Gerald Holt <dgh21989@hotmail.com>
2017-01-18 00:25:27 -0600

    Line of sight code looks much nicer in separate methods

--- src/boids/RobotPlayer.java
+++ src/boids/RobotPlayer.java
@@ -246,17 +246,26 @@ public strictfp class RobotPlayer {
         }
     }
 
-    private static boolean hasLineOfSight(BodyInfo target) {
-        BodyInfo[] things = concatArrays(rc.senseNearbyTrees(), rc.senseNearbyRobots());
+    private static boolean obstructsLineOfSight(BodyInfo target, BodyInfo thing) {
+        return distanceToIntersection(rc.getLocation(), target.getLocation(), thing) <= 0;
+    }
+
+    private static boolean isLineOfSightObstructedBy(BodyInfo target, BodyInfo[] things) {
         for (BodyInfo thing : things) {
-            if (thing.getID() == target.getID() || rc.getID() == target.getID()) {
-                continue;
-            }
-            if (distanceToIntersection(rc.getLocation(), target.getLocation(), thing) >= 0) {
-                return false;
+            if (thing.getID() != target.getID() && rc.getID() != target.getID() && obstructsLineOfSight(target, thing)) {
+                return true;
             }
         }
-        return true;
+        return false;
+    }
+
+    private static boolean hasLineOfSight(BodyInfo target) {
+        return hasLineOfSight(target, false);
+    }
+
+    private static boolean hasLineOfSight(BodyInfo target, boolean ignoreRobots) {
+        return !isLineOfSightObstructedBy(target, rc.senseNearbyTrees()) &&
+                (ignoreRobots || !isLineOfSightObstructedBy(target, rc.senseNearbyRobots()));
     }
 
     private static void attackClosestEnemy() throws GameActionException {
